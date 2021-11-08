1f6cc225e7927991cf359703f6c5541b5b9d8eff
eugeneyap <49102312+eugeneyap@users.noreply.github.com>
2021-11-08 23:15:22 +0800

    update viewReservation() and viewSalesRevenue to print data from txt

--- src/entity/Staff.java
+++ src/entity/Staff.java
@@ -1,5 +1,8 @@
 package entity;
 
+import java.util.Scanner;
+import java.nio.file.Paths;
+
 public class Staff {
 
 	private int staffID;
@@ -9,12 +12,35 @@ public class Staff {
 
 	public void viewReservations() {
 		// TODO - implement Staff.viewReservations
-		
+		// https://java-programming.mooc.fi/part-4/3-files-and-reading-data
+		// we create a scanner for reading the file
+		try (Scanner scanner = new Scanner(Paths.get("reservations.txt"))) {
+			// we read the file until all lines have been read
+			while (scanner.hasNextLine()) {
+				// we read one line
+				String row = scanner.nextLine();
+				// we print the line that we read
+				System.out.println(row);
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 
 	public void viewSalesRevenue() {
 		// TODO - implement Staff.viewSalesRevenue
-		
+		// https://java-programming.mooc.fi/part-4/3-files-and-reading-data
+		try (Scanner scanner = new Scanner(Paths.get("revenue.txt"))) {
+			// we read the file until all lines have been read
+			while (scanner.hasNextLine()) {
+				// we read one line
+				String row = scanner.nextLine();
+				// we print the line that we read
+				System.out.println(row);
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 
 	public String getName() {
@@ -22,7 +48,7 @@ public class Staff {
 		return this.staffName;
 	}
 
-	public String getGender() {
+	public boolean getGender() {
 		// TODO - implement Staff.getGender
 		return this.gender;
 	}
