2d0b82490bdce299a58a6c90c835f490dc010bb9
GallonKnight <tomomitomoe@gmail.com>
2021-11-13 16:53:14 +0800

    Done Java Doc for Table and Reservation

--- src/entity/Reservation.java
+++ src/entity/Reservation.java
@@ -1,19 +1,50 @@
-// no man's repsonsiblity :X
 package entity;
-
 import java.time.*;
+
+/**
+ * Represents a reservation in a reservation list.
+ * A reservation holds a table object.
+ * @author Yeo Kai Liang, Jasper
+ * @version 1.0
+ * @since 2021-10-15
+ */
 
 
 public class Reservation {
 
-	//private String bookingName; // change to Customer
-	//private int contact;
+	/**
+	 * The full name of customer
+	 */
 	private String cusName;
+	/**
+	 * The contact number of customer
+	 */
 	private int cusContact;
+	/**
+	 * The reservation of number of people for 1 table
+	 */
 	private int pax;
+	/**
+	 * The expected arrival time of customer
+	 */
 	private LocalTime bookingTime;
+	/**
+	 * The table reserved for customer
+	 */
 	private Table customerTable;
 
+
+	/**
+	 * Creates a new Reservation with the  given:
+	 * customer name, customer contact number, number of people, reservation time, reserved table.
+	 * The customer name can inlcude both first and last name, or either of them.
+	 * @param cusName		This is customer name
+	 * @param cusContact	This is customer contact
+	 * @param p				This is number of people (pax)
+	 * @param bT			This is reservation time slot
+	 * @param table			This is table object reserved for customer
+	 * 
+	 */
 	public Reservation(String cusName, int cusContact, int p, LocalTime bT, Table table){
 		this.cusName = cusName;
 		this.cusContact = cusContact;
@@ -22,25 +53,46 @@ public class Reservation {
 		this.customerTable = table;
 	}
 
+	/**
+	 * Gets the name of this customer.
+	 * @return this customer's name
+	 */
 	public String getCusName(){
 		return this.cusName;
 	}
+	/**
+	 * Gets the contact number of this customer.
+	 * @return this customer's contact number
+	 */
 	public int getCusContact(){
 		return this.cusContact;
 	}
+	/**
+	 * Gets the number of people reserving for a table
+	 * @return this customer's head count
+	 */
 	public int getPax(){
 		return this.pax;
 	}
 
+	/**
+	 * Gets the customer's expected arrival time.
+	 * @return this customer's expected arrival time
+	 */
 	public LocalTime getBookingTime(){
 		return this.bookingTime;
 	}
-
+	/**
+	 * Gets the table reserved for this Customer.
+	 * @return the table object reserved for this customer
+	 */
 	public Table getTable(){
 		return this.customerTable;
 	}
 
-
+	/**
+	 * Display this reservation details
+	 */
 	public void viewReservation() {
 		//do formatting later
 		System.out.print(this.bookingTime + " \t\t ");
--- src/entity/Table.java
+++ src/entity/Table.java
@@ -1,52 +1,92 @@
-// Jasper
 package entity;
 import java.util.ArrayList;
 
 import boundary.MainMenuUI;
 
+
+/**
+ * Represents a table in the Restaurant
+ * A table can be reserved or occupied by 1 customer.
+ * @author Yeo Kai Liang, Jasper
+ * @version 1.0
+ * @since 2021-10-15
+ */
+
+
 public class Table {
 
-	Reservation reservation;
+	/**
+	 * The identification number for this table
+	 */
 	private int tableID;
+	/**
+	 * The maximum number of people able to sit in this table
+	 */
 	private int seatCapacity;
+	/**
+	 * This table's occupied status
+	 */
 	private boolean occupied;
 
+	/**
+	 * Creates a new Table with the given table identification number and seat capacity
+	 * @param tID	This table's ID
+	 * @param seats This table's seat capacity
+	 */
 	public Table(int tID, int seats){
 		this.tableID = tID;
 		this.seatCapacity = seats;
-		this.occupied = false;  //default = false
+		this.occupied = false; 
 	}
 
+	/**
+	 * Gets ID of this table.
+	 * @return this table's name
+	 */
 	public int getTableID(){
 		return this.tableID;
 	}
-
+	/**
+	 * Get seat capacity of this table.
+	 * @return this table's seating capacity
+	 */
 	public int getSeatCapacity(){
 		return this.seatCapacity;
 	}
 
+	/**
+	 * Get occupied status of this table.
+	 * @return this table's occupied status
+	 */
 	public boolean getTableStatus(){
 		return this.occupied;
 	}
 
-	public void editTableID(int newID){
-		this.tableID = newID;
-	}
 
-	public void editSeatCapacity(int newCapacity){
-		this.seatCapacity = newCapacity;
-	}
-
-
+	/**
+	 * Set this table's occupied by customer status to TRUE
+	 */
 	public void bookTable() {  // should be no input instead; rmb change vpp also
 			this.occupied = true;
 		}
 
+	/**
+	 * Set this table's occupied by customer status to FALSE
+	 */
 	public void releaseTable() { // should be no input instead; rmb change vpp also
 			this.occupied = false;
 		}
 
 
+	/**
+	 * Initialise the tables in the restaurant
+	 * 
+	 * This table set up contains the following:
+	 * 5 tables that have 2 seat capacity
+	 * 5 tables that have 4 seat capacity
+	 * 5 tables that have 8 seat capacity
+	 * 3 tables that have 12 seat capcity
+	 */
 	public static void initTable(){
 		int i = 0;
 		ArrayList<Table> tables = MainMenuUI.table;
