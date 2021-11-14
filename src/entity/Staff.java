c740a61a69a8c64c629e074657390d8ffb71e257
eugeneyap <49102312+eugeneyap@users.noreply.github.com>
2021-11-14 19:51:15 +0800

    remove not needed functions that print revenue.txt and reservations.txt

--- src/entity/Staff.java
+++ src/entity/Staff.java
@@ -32,45 +32,6 @@ public class Staff {
 		this.gender = g;
 		this.jobTitle = jd;
 	}
-
-	/**
-	 * Prints all lines in reservations.txt 
-	 * @exception e If fail to read reservations.txt
-	 */
-	public static void viewReservations() {
-		// https://java-programming.mooc.fi/part-4/3-files-and-reading-data
-		// we create a scanner for reading the file
-		try (Scanner scanner = new Scanner(Paths.get("reservations.txt"))) {
-			// we read the file until all lines have been read
-			while (scanner.hasNextLine()) {
-				// we read one line
-				String row = scanner.nextLine();
-				// we print the line that we read
-				System.out.println(row);
-			}
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-	}
-
-	/**
-	 * Prints all lines in revenue.txt 
-	 * @exception e If fail to read revenue.txt
-	 */
-	public static void viewSalesRevenue() {
-		// https://java-programming.mooc.fi/part-4/3-files-and-reading-data
-		try (Scanner scanner = new Scanner(Paths.get("revenue.txt"))) {
-			// we read the file until all lines have been read
-			while (scanner.hasNextLine()) {
-				// we read one line
-				String row = scanner.nextLine();
-				// we print the line that we read
-				System.out.println(row);
-			}
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-	}
 	
 	/**
 	 * Get the Name of this Staff
